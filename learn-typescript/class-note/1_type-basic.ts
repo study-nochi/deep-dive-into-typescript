ce0132690d349aa8973ab4a39380eea8984f86f9
ainochi-kor <kthrkdals@gmail.com>
2023-10-02 22:07:02 +0900

    오랜만에 기초 공부

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,24 @@
+//* TS 문자열 선언
+let str: string = "hello";
+
+//* TS 숫자
+let num: number = 10;
+
+//* TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+let itmes: number[] = [1, 2, 3];
+
+//* TS 튜플
+let address: [string, number] = ["gangnam", 100];
+
+//* TS 객체
+let obj: object = {};
+let person: { name: string; age: number } = {
+  name: "capt",
+  age: 100,
+};
+
+//* TS 진위값
+let show: boolean = true;
+
