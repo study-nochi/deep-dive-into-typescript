17bcbb0563b6aa744308d86bcab392cb981d63ec
ainochi-kor <kthrkdals@gmail.com>
2023-10-03 00:52:20 +0900

    Add class

---
+++ learn-typescript/class-note/7_class.ts
@@ -0,0 +1,11 @@
+{
+  class Person {
+    private name: string;
+    public age: number;
+
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+}
